a8ae63c0a8028ac31756dc8314b25d2c5c44d430
yunguannjg <18757593931@163.com>
2025-05-15 10:20:24 +0800

    optimization: reduce unnecessary initialization time

--- pkg/httphelper/helper.go
+++ pkg/httphelper/helper.go
@@ -2,12 +2,21 @@ package httphelper
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func CreateHttpClient(proxy bool, proxyAddress string) *http.Client {
-	transport := &http.Transport{}
+	transport := &http.Transport{
+		MaxIdleConns:        10,
+		MaxIdleConnsPerHost: 10,
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+	}
 	if proxy {
 		proxyURL, err := url.Parse(proxyAddress)
 		if err != nil {
